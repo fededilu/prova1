7ed80cc3670ce733c1200018beaa39cd299bb0af
fededilu <federicodiluzio69@gmail.com>
2022-10-28 11:01:04 +0200

    modifca

--- src/main/java/org/example/helloworld.java
+++ src/main/java/org/example/helloworld.java
@@ -1,7 +1,7 @@
 package org.example;
 
-public class helloworld {
-    public static void main(){
+public class helloWorld {
+    public static void main(String[] args){
         System.out.println("Hello world!");
     }
 }
