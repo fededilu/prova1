443210deba91a5981962f14f2326019d06fa0dd5
fededilu <federicodiluzio69@gmail.com>
2022-10-28 11:02:55 +0200

    logger

--- src/main/java/org/example/helloworld.java
+++ src/main/java/org/example/helloworld.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class helloWorld {
     public static void main(String[] args){
-        System.out.println("Hello world!");
+        logger.log("Hello world!");
     }
 }
